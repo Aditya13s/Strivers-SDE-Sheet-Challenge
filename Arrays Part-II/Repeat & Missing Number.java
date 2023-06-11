da4baeda9d15cddf6d33df9cfe73b7cbddaca96b
Aditya <adityasharma131003@gmail.com>
2023-06-11 14:31:13 +0530

    Repeat & Missing Number(Optimal)

--- Arrays Part-II/Repeat & Missing Number.java
+++ Arrays Part-II/Repeat & Missing Number.java
@@ -25,3 +25,49 @@ public class Solution {
         return ans;
     }
 }
+/*
+ * Optimal
+ * TC = O(N)
+ * SC = O(1)
+ */
+public class Solution {
+    public static int[] missingAndRepeating(ArrayList<Integer> arr, int n) {
+        
+        int xor = 0;
+        for(int i=0; i<n; i++) {
+            xor ^= arr.get(i);
+            xor ^= i+1;
+        }
+
+        int rsb = xor & -xor;
+        int x=0,y=0;
+        for(int i=1; i<=n; i++) {
+            if((rsb & arr.get(i-1)) == 0){
+                x ^= arr.get(i-1);
+            } else {
+                y ^= arr.get(i-1);
+            }
+            if((i & rsb)==0) {
+                x ^= i;
+            } else {
+                y ^= i;
+            }
+        }
+
+        int[] ans = new int[2];
+        for(int i=0; i<n; i++) {
+            if(x == arr.get(i)) {
+                ans[1] = x;
+                ans[0] = y;
+                break;
+            }else if(y == arr.get(i)) {
+                ans[0] = x;
+                ans[1] = y;
+                break;
+            }
+        }
+        
+        return ans;
+    }
+
+}

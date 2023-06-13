500f61cdfa2c4e7464e880a701282379b520291d
Aditya <adityasharma131003@gmail.com>
2023-06-14 00:38:43 +0530

    Grid Unique Paths

--- Arrays Part-III/Grid Unique Paths.java
+++ Arrays Part-III/Grid Unique Paths.java
@@ -14,3 +14,46 @@ class Solution {
         else return getPaths(r, c+1, m, n) + getPaths(r+1, c, m, n);
     }
 }
+
+
+/*
+ * Better(Dynamic Programming)
+ * TC = O(n*m)
+ * SC = O(n*m)
+ */
+class Solution {
+    public int uniquePaths(int m, int n) {
+        int dp[][] = new int[m][n];
+        for(int[] row: dp) {
+            Arrays.fill(row, -1);
+        }
+        int paths = getPaths(0,0,m-1, n-1, dp);
+        return paths;
+    }
+    public int getPaths(int r, int c, int m, int n, int dp[][]) {
+        if(r == m && c == n) return 1;
+        if(r>m || c > n) return 0;
+
+        if(dp[r][c] != -1) return dp[r][c]; 
+        else return dp[r][c] = getPaths(r, c+1, m, n, dp) + getPaths(r+1, c, m, n, dp);
+    }
+}
+
+/*
+ * Optimal(Combinatorics)
+ * TC = O(n) or O(m)
+ * SC = O(1)
+ */
+class Solution {
+    public int uniquePaths(int m, int n) {
+        int N = m+n-2;
+        int r= m-1;
+        double res = 1;
+
+        for(int i=1; i<=r; i++) {
+            res = res *(N-r+i)/i;
+        }
+        return (int)res;
+    }
+    
+}

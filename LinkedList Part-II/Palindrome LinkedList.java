74078d5c08da71d8d171e4b436953245e147cfcb
Aditya <adityasharma131003@gmail.com>
2023-08-01 01:06:03 +0530

    Palindrome LinkedList Optimal Approach

--- LinkedList Part-II/Palindrome LinkedList.java
+++ LinkedList Part-II/Palindrome LinkedList.java
@@ -26,3 +26,44 @@ class Solution {
     }
 }
 
+
+/*
+ * Optimal
+ * TC = O(N)
+ * SC = O(1)
+ */
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        if(head==null||head.next==null) return true;
+        
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while(fast.next != null && fast.next.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+
+        slow.next = reverseList(slow.next);
+        slow = slow.next;
+        while(slow != null) {
+            if(head.val != slow.val) {
+                return false;
+            }
+            head = head.next;
+            slow = slow.next;
+        }
+        return true;
+    }
+
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        while(head != null) {
+            ListNode tmp = head.next;
+            head.next = prev;
+            prev = head;
+            head = tmp;
+        }
+        return prev;
+    }
+}

16939d5cb19d92f25fea3494f1b284ef1fea87c9
Aditya <adityasharma131003@gmail.com>
2023-08-18 15:46:09 +0530

    Combination Sum II Solution

--- Recursion/Combination Sum II.java
+++ Recursion/Combination Sum II.java
@@ -29,3 +29,34 @@ class Solution {
         }
     }
 }
+
+
+/*
+ * Optimal Approach 2
+ * TC = O(2^N)
+ * SC = O(N)
+ */
+class Solution {
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        Arrays.sort(candidates);
+        List<List<Integer>> result = new ArrayList<>();
+        combination(0, candidates, target, new ArrayList<Integer>(), result);
+        return result;
+    }
+
+    public void combination(int index, int[] candidates, int target,  List<Integer> list, List<List<Integer>> result) {
+        if(target == 0) {
+            result.add(new ArrayList<>(list));
+            return;
+        }
+
+        for(int i=index; i<candidates.length; i++) {
+            if(i != index && candidates[i] == candidates[i-1]) continue;
+            if(candidates[i] > target) break;
+
+            list.add(candidates[i]);
+            combination(i+1, candidates, target - candidates[i], list, result);
+            list.remove(list.size() - 1);
+        }
+    }
+}

8cbcf2f30c5485a4bb3911bfd193bee5b7463304
Aditya <adityasharma131003@gmail.com>
2023-08-21 01:53:18 +0530

    Palindrome Patitioning Solution

--- Recursion/Palindrome Partitioning.java
+++ Recursion/Palindrome Partitioning.java
@@ -1,1 +1,35 @@
- 
+/*
+ * TC = O(2^N)
+ * SC = O(N)
+ */
+class Solution {
+    public List<List<String>> partition(String s) {
+        List<List<String>> result = new ArrayList<>();
+        substring(0, s, new ArrayList<String>(), result);
+        return result;
+        
+    }
+
+    public void substring(int idx, String s, List<String> substring, List<List<String>> result) {
+        if (idx == s.length()) {
+            result.add(new ArrayList<>(substring));
+            return;
+        }
+
+        for(int i=idx; i<s.length(); i++) {
+            if(isPalindrome(s, idx, i)) {
+                substring.add(s.substring(idx, i+1));
+                substring(i+1, s, substring, result);
+                substring.remove(substring.size() -1);
+            }
+        }
+
+    }
+
+    public boolean isPalindrome(String s, int l, int r) {
+        while(l<=r) {
+            if(s.charAt(l++) != s.charAt(r--)) return false;
+        }
+        return true;
+    }
+}

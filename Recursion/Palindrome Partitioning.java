3ee0d3f12a22d5101ccb3860d58e9180d302c1ef
Aditya <adityasharma131003@gmail.com>
2023-08-21 19:36:37 +0530

    Palindrome Partitioning Dp solution

--- Recursion/Palindrome Partitioning.java
+++ Recursion/Palindrome Partitioning.java
@@ -33,3 +33,40 @@ class Solution {
         return true;
     }
 }
+
+/*
+ * Dynamic Programming
+ * TC = O(N^2)
+ * SC = O(N^2)
+ */
+class Solution {
+    public List<List<String>> partition(String s) {
+        int n = s.length();
+        boolean[][] dp = new boolean[n][n];
+        for (int len = 1; len <= n; len++) {
+            for (int start = 0; start <= n - len; start++) {
+                int end = start + len - 1;
+                dp[start][end] = (s.charAt(start) == s.charAt(end)) && (len <= 2 || dp[start + 1][end - 1]);
+            }
+        }
+        
+        List<List<String>> result = new ArrayList<>();
+        generatePartitions(0, s, new ArrayList<String>(), dp, result);
+        return result;
+    }
+
+    public void generatePartitions(int start, String s, List<String> partition, boolean[][] dp, List<List<String>> result) {
+        if (start == s.length()) {
+            result.add(new ArrayList<>(partition));
+            return;
+        }
+
+        for (int end = start; end < s.length(); end++) {
+            if (dp[start][end]) {
+                partition.add(s.substring(start, end + 1));
+                generatePartitions(end + 1, s, partition, dp, result);
+                partition.remove(partition.size() - 1);
+            }
+        }
+    }
+}

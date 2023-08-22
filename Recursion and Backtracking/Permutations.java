aa2061f615d4cb89bbb087add9136ac6fd55587b
Aditya <adityasharma131003@gmail.com>
2023-08-23 00:45:09 +0530

    Permutations Optimal Soln

--- Recursion and Backtracking/Permutations.java
+++ Recursion and Backtracking/Permutations.java
@@ -1,4 +1,5 @@
 /*
+ * Changing Array to List
  * TC = O(N!)
  * SC = O(N)
  */
@@ -28,5 +29,42 @@ class Solution {
             permutation.remove(permutation.size() -1);
             nums.add(i, num);
         }
+    }
+}
+
+/*
+ * Optimal(Without Changing array to list)
+ * TC = O(N!)
+ * SC = O(N)
+ */
+class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        permutations(0, nums, result);
+        return result;
+    }
+
+    public void permutations(int index, int[] nums, List<List<Integer>> result) {
+        if(index == nums.length) {
+            List<Integer> permutation = new ArrayList<>();
+            for (int num : nums) {
+                permutation.add(num);
+            }
+            result.add(permutation);
+
+            return;
+        }
+
+        for(int i=index; i<nums.length; i++) {
+            swap(nums, i, index);
+            permutations(index+1, nums, result);
+            swap(nums, i, index);
+
+        }
+    }
+    public void swap(int[] nums, int l, int r) {
+        int temp = nums[l];
+        nums[l] = nums[r];
+        nums[r] = temp;
     }
 }
